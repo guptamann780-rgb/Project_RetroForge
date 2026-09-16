77b774799c53ecf62eb0c8892d4615e22ba3a60d
Mann Gupta <guptamann780@gmail.com>
2026-09-16 11:28:47 +0000

    made gamecube stub

---
+++ pokefirered/host_src/gamecube_stub.c
@@ -0,0 +1,17 @@
+#include "libgcnmultiboot.h"
+
+void GameCubeMultiBoot_Main(struct GcmbStruct *pStruct)
+{
+	return;
+}
+
+
+void GameCubeMultiBoot_ExecuteProgram(struct GcmbStruct *pStruct)
+{
+	return;	
+}
+
+void GameCubeMultiBoot_Init(struct GcmbStruct *pStruct)
+{
+	return;
+}
